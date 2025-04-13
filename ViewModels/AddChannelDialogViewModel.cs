53f4fedd5112633b9e181589275010ee30e5d900
B-Cut <cgoncalves@id.uff.br>
2025-04-13 19:16:19 -0300

    Added base to add new channels. More styling to AddChannelDialog.

--- ViewModels/AddChannelDialogViewModel.cs
+++ ViewModels/AddChannelDialogViewModel.cs
@@ -1,12 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using HanumanInstitute.MvvmDialogs;
 using RssReader.Services;
 
 namespace RssReader.ViewModels;
 
-public class AddChannelDialogViewModel : ViewModelBase, IModalDialogViewModel
+public partial class AddChannelDialogViewModel : ViewModelBase, IModalDialogViewModel
 {
     private ChannelManagementService _channelManager;
 
+    // We can keep it as bool, change to Enum if more channel adding methods are introduced
+    [ObservableProperty]
+    private bool _addViaUrl = true;
+
+    [ObservableProperty]
+    private string _inputContent = string.Empty;
+    
+    // This is bad OOP, but it will do for now
+    [RelayCommand]
+    private async Task AddNewChannel()
+    {
+        Console.WriteLine(InputContent);
+        Console.WriteLine($"Add via URL: {AddViaUrl}");
+        if(string.IsNullOrWhiteSpace(InputContent)) return;
+        // TODO: Implement error messages
+
+        /*
+        if (AddViaUrl)
+        {
+            await _channelManager.ReceiveFromUrl(InputContent);
+        }
+        else
+        {
+            await _channelManager.ReceiveFromFile(InputContent);
+        }*/
+    }
+    
     public AddChannelDialogViewModel(ChannelManagementService channelManager)
     {
         _channelManager = channelManager;
--- Views/AddChannelDialogView.axaml.cs
+++ Views/AddChannelDialogView.axaml.cs
@@ -1,12 +1,14 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace RssReader.Views;
 
 public partial class AddChannelDialogView : UserControl
 {
+    
     public AddChannelDialogView()
     {
         InitializeComponent();
